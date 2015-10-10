1baaefb787b172c7cb2ef7d9e652d72f990ac1fe
Hernán Olmedo <hernan.olmedo@usach.cl>
2015-10-10 01:56:41 -0300

    2do profiling

--- Laboratorio 3/script.sh
+++ Laboratorio 3/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/max/Documents/Laboratorio-3/Laboratorio 3
+cd /home/max/Documents/Laboratorio-3/Laboratorio\ 3
 gcc -Wall -O0 -pg main.c -o main
 ./main -n 0.5
 gprof main gmon.out > analysis
